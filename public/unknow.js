64a1590ed9535436434be224cf00036cb1fe9f2a
myomyintaung1411 <myomyintaung1411@gmail.com>
2024-06-13 13:32:12 +0800

    lasted

--- public/unknow.js
+++ public/unknow.js
@@ -1,12 +1,12 @@
 
 // //web one
 const global = {
-    reg:'https://hjtg.th371.com/?InvCode=GO40906',
-    old_pc_game_url:'https://pc1.th371.com/',
-    new_pc_game_url:'https://pc2.th371.com/',
-    h5_game_url:'https://h5.th371.com/',
-    ad:"https://ht.th371.com/",
-    app_down:"https://download.yx753.com/",
+    reg:'https://hjtg.hjbc666.com/?InvCode=GO40906',
+    old_pc_game_url:'https://pc1.hjbc666.com/',
+    new_pc_game_url:'https://pc2.hj812.vip/',
+    h5_game_url:'https://h5.hj812.vip/',
+    ad:"https://ht.hjbc666.com/",
+    app_down:"https://download.hjbc66.com/",
     chat_app:"http://www.cx6699.top/",
     small_game:'http://sg.royalgaming.today',
     service1:"https://hjkf88.cn/#/?visiter_id=&avatar=&business_id=1&groupid=1&special=80",
